989c8e304e4f89f05764f690d4ee52384d118575
JonathanPabon-Dev <jonathanpabon.dev@gmail.com>
2024-04-30 01:25:54 -0500

    Primer commit con creación del proyecto basado en la plantilla y desarrollo de pasos 1 y 2 del feedback

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Feedback from './Feedback/Feedback';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Feedback />
+      </div>
+    );
+  }
+}
+
+export default App;
